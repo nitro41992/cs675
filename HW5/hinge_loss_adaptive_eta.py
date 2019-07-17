cacbf5dcada01a46739ea03a121857b0969c0e52
nitro41992 <nick.k@iton.net>
2019-07-16 22:07:39 -0400

    Task: HW5 Progress

---
+++ HW5/hinge_loss_adaptive_eta.py
@@ -0,0 +1,152 @@
+import sys
+import math
+import random
+
+
+# Read File
+datafile = sys.argv[1]
+f = open(datafile)
+data = []
+i = 0
+l = f.readline()
+while (l != ''):
+    a = l.split()
+    l2 = []
+    for j in range(0, len(a), 1):
+        l2.append(float(a[j]))
+    data.append(l2)
+    l = f.readline()
+    data[i].append(1)
+    i += 1
+
+rows = len(data)
+cols = len(data[0])
+f.close()
+# print(f'Data: {data}')
+# print(f'rows: {rows}')
+# print(f'cols: {cols}')
+
+# Read Labels
+label_data = sys.argv[2]
+f = open(label_data)
+labels = {}
+
+l = f.readline()
+while(l != ''):
+    a = l.split()
+    labels[int(a[1])] = int(a[0])
+    if(labels[int(a[1])] == 0):
+        labels[int(a[1])] = -1
+    else:
+        labels[int(a[1])] = 1
+    l = f.readline()
+f.close()
+
+# print(data)
+# print(labels)
+
+# dot product function
+
+
+def dot_product(refw, refx):
+    dot_product = 0
+    for j in range(0, cols, 1):
+        dot_product += refw[j]*refx[j]
+    return dot_product
+
+
+# Initialize w
+w = []
+for j in range(0, cols, 1):
+    w.append(float(0.02*random.uniform(0, 1) - 0.01))
+
+
+# dellf descent iteration
+eta = 0.001
+diff = 1
+error = 0
+stop = 0.001
+count = 0
+
+# compute dellf and error
+while (diff > stop):
+    dellf = []
+    dellf.extend(0 for _ in range(cols))
+
+    for i in range(0, rows, 1):
+        if (labels.get(i) != None):
+            a = labels[i] * dot_product(w, data[i])
+            for j in range(0, cols):
+                if a < 1:
+                    dellf[j] += -(labels[i] * data[i][j])
+                else:
+                    dellf[j] += 0
+
+    # print(f'dellf: {dellf}')
+    # adaptive hinge
+    eta_list = [1, .1, .01, .001, .0001, .00001, .000001,
+                .0000001, .00000001, .000000001, .0000000001, .00000000001]
+    bestobj = 1000000000000
+
+    for k in range(0, len(eta_list), 1):
+        eta = eta_list[k]
+
+        # update w
+        for j in range(0, cols, 1):
+            w[j] -= eta * dellf[j]
+
+        # calculate error
+        error = 0
+        for i in range(0, rows):
+            if (labels.get(i) != None):
+                error += max(0, 1 - (labels.get(i))
+                             * dot_product(w, data[i]))
+
+        obj = error
+        if obj < bestobj:
+            bestobj = obj
+            best_eta = eta
+
+        # update w
+        for j in range(0, cols, 1):
+            w[j] += eta * dellf[j]
+
+    if best_eta != None:
+        eta = best_eta
+    # update w
+    for j in range(0, cols, 1):
+        w[j] -= eta*dellf[j]
+    # print(f'w[j]: {w[j]}')
+
+    prevObj = error
+    error = 0
+    # compute error
+
+    for i in range(0, rows, 1):
+        if (labels.get(i) != None):
+            error += max(0, 1 - (labels.get(i)) * dot_product(w, data[i]))
+
+    diff = abs(prevObj - error)
+
+# print(f'diff: {diff}')
+# print("error: ", error)
+
+print(f'w =  {w[0:2]}')
+# distance from origin calculation (pythagorean theorem)
+normw = 0
+for j in range(0, cols-1, 1):
+    normw += w[j]**2
+print(f'w0 = {w[2]}')
+
+normw = math.sqrt(normw)
+origin_distance = abs(w[len(w)-1]/normw)
+print('distance from origin =  ', origin_distance)
+
+# prediction
+for i in range(0, rows, 1):
+    if (labels.get(i) == None):
+        dp = dot_product(w, data[i])
+        if dp > 0:
+            print("1,", i)
+        else:
+            print("0,", i)
---
+++ HW5/least_squares_adaptive_eta.py
@@ -0,0 +1,146 @@
+import sys
+import math
+import random
+
+
+# Read File
+datafile = sys.argv[1]
+f = open(datafile)
+data = []
+i = 0
+l = f.readline()
+while (l != ''):
+    a = l.split()
+    l2 = []
+    for j in range(0, len(a), 1):
+        l2.append(float(a[j]))
+    data.append(l2)
+    l = f.readline()
+    data[i].append(1)
+    i += 1
+rows = len(data)
+cols = len(data[0])
+f.close()
+# print(f'Data: {data}')
+# print(f'rows: {rows}')
+# print(f'cols: {cols}')
+
+# Read Labels
+label_data = sys.argv[2]
+f = open(label_data)
+labels = {}
+
+
+l = f.readline()
+while(l != ''):
+    a = l.split()
+    labels[int(a[1])] = int(a[0])
+    if(labels[int(a[1])] == 0):
+        labels[int(a[1])] = -1
+    l = f.readline()
+f.close()
+
+# print(data)
+# print(labels)
+
+# dot product function
+
+
+def dot_product(refw, refx):
+    dot_product = 0
+    for j in range(0, cols, 1):
+        dot_product += refw[j]*refx[j]
+    return dot_product
+
+
+# Initialize w
+w = []
+for j in range(0, cols, 1):
+    w.append(float(0.02*random.uniform(0, 1) - 0.01))
+
+
+# dellf descent iteration
+eta = 0.001
+stop = 0.001
+error = 0
+
+# compute dellf and error
+while True:
+    dellf = []
+    dellf.extend(0 for _ in range(cols))
+    prev_iter_error = error
+    for i in range(0, rows, 1):
+        if (labels.get(i) != None):
+            dp = dot_product(w, data[i])
+            for j in range(0, cols, 1):
+                dellf[j] += float((labels[i]-dp)*data[i][j])
+
+    # print(f'dellf: {dellf}')
+    # adaptive hinge
+    eta_list = [1, .1, .01, .001, .0001, .00001, .000001,
+                .0000001, .00000001, .000000001, .0000000001, .00000000001]
+    bestobj = 1000000000000
+
+    for k in range(0, len(eta_list), 1):
+        eta = eta_list[k]
+
+        # update w
+        for j in range(0, cols, 1):
+            w[j] -= eta * dellf[j]
+
+        # calculate error
+        error = 0
+        for i in range(0, rows):
+            if (labels.get(i) != None):
+                error += max(0, 1 - (labels.get(i))
+                             * dot_product(w, data[i]))
+
+        obj = error
+        if obj < bestobj:
+            bestobj = obj
+            best_eta = eta
+
+        # update w
+        for j in range(0, cols, 1):
+            w[j] += eta * dellf[j]
+
+    if best_eta != None:
+        eta = best_eta
+
+    # update w
+    for j in range(0, cols, 1):
+        w[j] += eta*dellf[j]
+    # print(f'w[j]: {w[j]}')
+
+    # compute error
+    error = 0
+    for i in range(0, rows, 1):
+        if (labels.get(i) != None):
+            error += (labels[i] - dot_product(w, data[i]))**2
+
+    # print("error: ", error)
+    # print("prevError", prev_iter_error)
+    # print("error diff", abs(prev_iter_error - error))
+
+    if abs(prev_iter_error - error) <= stop:
+        break
+
+# print(best_eta)
+# distance from origin calculation (pythagorean theorem)
+normw = 0
+for j in range(0, cols-1, 1):
+    normw += w[j]**2
+    print(f'w{j}: {abs(w[j])}')
+
+normw = math.sqrt(normw)
+origin_distance = abs(w[len(w)-1]/normw)
+print('distance from origin: ', origin_distance)
+
+# prediction
+for i in range(0, rows, 1):
+    if (labels.get(i) == None):
+        dp = dot_product(w, data[i])
+        if dp > 0:
+            print("1,", i)
+        else:
+            print("0,", i)
