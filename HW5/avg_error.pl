15088f90fee949e07efd477c9fdd14d0c8248e4d
nitro41992 <nick.k@iton.net>
2019-07-24 23:03:59 -0400

    Task: Removed Large files. Added HW 5 and 6

--- HW5/avg_error.pl
+++ HW5/avg_error.pl
@@ -1,10 +1,10 @@
 $mean = 0;
 $data = shift;
-$dir=$data."_std";
+$dir=$data;
 #$dir=$data;
 for(my $i=0; $i<10; $i++){
-  system("python nm_fall17.py $dir/$data.data $dir/$data.trainlabels.$i > nm_out.$data");
-  $err[$i] = `perl error.pl $dir/$data.labels nm_out.$data`;
+  system("python least_squares_adaptive_eta.py $dir/$data.data $dir/$data.trainlabels.$i > least_squares_out.$data");
+  $err[$i] = `perl error.pl $dir/$data.labels least_squares_out.$data`;
   chomp $err[$i];
   print "$err[$i]\n";
   $mean += $err[$i];
@@ -16,13 +16,13 @@ for(my $i=0; $i<10; $i++){
 }
 $sd /= 10;
 $sd = sqrt($sd);
-print "Nearest means error = $mean ($sd)\n";
+print "Least Squares Error = $mean ($sd)\n";
 
 #q^
 $mean = 0;
 for(my $i=0; $i<10; $i++){
-  system("python3 nb.py $dir/$data.data $dir/$data.trainlabels.$i > nb_out.$data");
-  $err[$i] = `perl error.pl $dir/$data.labels nb_out.$data`;
+  system("python hinge_adaptive_eta.py $dir/$data.data $dir/$data.trainlabels.$i > hinge_out.$data");
+  $err[$i] = `perl error.pl $dir/$data.labels hinge_out.$data`;
   chomp $err[$i];
   print "$err[$i]\n";
   $mean += $err[$i];
@@ -35,67 +35,5 @@ for(my $i=0; $i<10; $i++){
 }
 $sd /= 10;
 $sd = sqrt($sd);
-print "Naive Bayes error = $mean ($sd)\n";
+print "Hinge Adaptive Error = $mean ($sd)\n";
 #^if 0;
-
-#q^
-$mean = 0;
-for(my $i=0; $i<10; $i++){
-  system("python3 perceptron.py $dir/$data.data $dir/$data.trainlabels.$i $eta $stop > p_out.$data");
-  $err[$i] = `perl error.pl $dir/$data.labels p_out.$data`;
-  print $err[$i];
-  chomp $err[$i];
-  $mean += $err[$i];
-}
-$mean /= 10;
-$sd = 0;
-for(my $i=0; $i<10; $i++){
-  $sd += ($err[$i]-$mean)**2;
-}
-$sd /= 10;
-$sd = sqrt($sd);
-print "Perceptron (eta=$eta) error = $mean ($sd)\n";
-#^if 0;
-
-q^
-$mean = 0;
-for(my $i=0; $i<10; $i++){
-  system("python3 hinge.py $dir/$data.data $dir/$data.trainlabels.$i $eta $stop > p_out");
-  $err[$i] = `perl error.pl $dir/$data.labels p_out`;
-  print $err[$i];
-  chomp $err[$i];
-  $mean += $err[$i];
-}
-$mean /= 10;
-$sd = 0;
-for(my $i=0; $i<10; $i++){
-  $sd += ($err[$i]-$mean)**2;
-}
-$sd /= 10;
-$sd = sqrt($sd);
-print "Hinge (eta=$eta) error = $mean ($sd)\n";
-^if 0;
-
-q^
-$mean = 0;
-for(my $i=0; $i<10; $i++){
-  ##Create the training data and labels for SVM
-
-  ##Obtain the cross-validated value of C
-  $C = `perl cv-svm.pl data_cv labels_cv`;
-
-  ##Predict with that value of C
-  system("perl run_svm_light.pl $data.data $data.trainlabels.$i $C");
-  $err[$i] = `perl error.pl $data.labels svmpredictions`;
-  chomp $err[$i];
-  $mean += $err[$i];
-}
-$mean /= 10;
-$sd = 0;
-for(my $i=0; $i<10; $i++){
-  $sd += ($err[$i]-$mean)**2;
-}
-$sd /= 10;
-$sd = sqrt($sd);
-print "SVM error = $mean ($sd)\n";
-^if 0;
